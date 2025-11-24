09a8e021d7e956118f818084ddd1aa5d4217bf42
samsehgal3100-cyber <samsehgal3100@gmail.com>
2025-11-24 22:15:56 +0530

    Create Day03_A_Temperature.c

---
+++ Day03_A_Temperature.c
@@ -0,0 +1,19 @@
+/*
+Q5 (User Inputs, Operations & Output)
+Write a program to convert temperature from Celsius to Fahrenheit.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    double c;
+    printf("Enter temperature in Celsius: ");
+    if (scanf("%lf", &c) != 1) {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    double f = (c * 9.0 / 5.0) + 32.0;
+    printf("Fahrenheit: %.2f\n", f);
+    return 0;
+}
