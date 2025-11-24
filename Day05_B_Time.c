2dc790b7badb4cbae941befe503bc68d2c2fa20b
samsehgal3100-cyber <samsehgal3100@gmail.com>
2025-11-24 22:32:34 +0530

    Create Day05_B_Time.c

---
+++ Day05_B_Time.c
@@ -0,0 +1,20 @@
+/*
+Q10 (User Inputs, Operations & Output)
+Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    int total_seconds, hours, minutes, seconds;
+
+    printf("Enter time in seconds: ");
+    scanf("%d", &total_seconds);
+
+    hours = total_seconds / 3600;
+    minutes = (total_seconds % 3600) / 60;
+    seconds = total_seconds % 60;
+
+    printf("Time: %02d:%02d:%02d\n", hours, minutes, seconds);
+    return 0;
+}
