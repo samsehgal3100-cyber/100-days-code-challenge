a81d409df97eb122ea04df5b31a125780c9375fd
samsehgal3100-cyber <samsehgal3100@gmail.com>
2025-11-24 22:30:51 +0530

    Create Day04_B_Natural.c

---
+++ Day04_B_Natural.c
@@ -0,0 +1,19 @@
+/*
+Q8 (User Inputs, Operations & Output)
+Write a program to find and display the sum of the first n natural numbers.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    int n, sum = 0;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    printf("Sum of first %d natural numbers = %d\n", n, sum);
+    return 0;
+}
